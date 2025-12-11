48873108dc1a8e84f878a143cce91e9de9f49b3d
nicolemorenas <morenas@shernet.sheridancollege.ca>
2025-12-11 09:24:50 -0500

    Changed API Key

--- app/src/main/java/sheridan/dheripu/fitnutrition/data/RetrofitInstance.kt
+++ app/src/main/java/sheridan/dheripu/fitnutrition/data/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private val client = OkHttpClient.Builder().addInterceptor { chain ->
         val request = chain.request().newBuilder()
-            .addHeader("X-RapidAPI-Key", "978d400a3amsh7cce0b13181bf48p177261jsn6990a1cd42cb")
+            .addHeader("X-RapidAPI-Key", "192886c2e0mshf67522f464867ebp14f9e6jsn27c4a67345a9")
             .addHeader("X-RapidAPI-Host", "exercisedb.p.rapidapi.com")
             .build()
         chain.proceed(request)
